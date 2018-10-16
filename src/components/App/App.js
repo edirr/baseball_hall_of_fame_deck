4ec4cdf972deb0e9e4b9f19e014497bd8189ea45
Daniel McKemie <daniel.mckemie@gmail.com>
2018-10-16 16:31:44 -0400

    layout cleanup

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,9 @@
-import React, { Component } from "react";
-import classnames from "classnames";
-import { Link } from "react-router-dom";
-import logo from "./logo.svg";
-import Nav from "../Nav";
-import "./style.css";
+import React, { Component } from 'react';
+import classnames from 'classnames';
+import { Link } from 'react-router-dom';
+import Nav from '../Nav';
+import SearchForm from '../SearchForm'
+import './style.css';
 import 'bulma';
 
 class App extends Component {
@@ -11,30 +11,8 @@ class App extends Component {
     const { className, ...props } = this.props;
     return (
       <div className={classnames("App", className)} {...props}>
-        <Nav />
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React - Fullstack!</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <Link to="about">
-          <button>Test React Router</button>
-        </Link>
-        <br />
-        <br />
-        <button onClick={this.props.actions.expressTest}>
-          Test if Express is working
-        </button>
-        <br />
-        <br />
-        <button onClick={this.props.actions.dbTest}>
-          Test if Express and Sequelize are working
-        </button>
-        <div style={{ padding: "30px" }}>{this.props.results}</div>
-      </div>
-      <SearchFrom/>
+        <Nav />     
+      <SearchForm />
       </div>
     );
   }
