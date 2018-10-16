2ac1eea2bc737354d5afd12b70916020827bcd38
Eric Dirr <edirr92@aol.com>
2018-10-16 18:25:33 -0400

    CardImg and CardList

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,7 +2,9 @@ import React, { Component } from 'react';
 import classnames from 'classnames';
 import { Link } from 'react-router-dom';
 import Nav from '../Nav';
-import SearchForm from '../SearchForm'
+import SearchForm from '../SearchForm';
+import CardImg from '../CardImg/';
+import CardList from '../CardList';
 import './style.css';
 import 'bulma';
 
@@ -11,8 +13,9 @@ class App extends Component {
     const { className, ...props } = this.props;
     return (
       <div className={classnames("App", className)} {...props}>
-        <Nav />     
+        <Nav />
       <SearchForm />
+      <CardList />
       </div>
     );
   }
