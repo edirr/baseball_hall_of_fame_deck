a8a3001ac7ea10c13a10f744437d14ce92e47888
Misha Holzman <mcholzman1@gmail.com>
2018-10-16 23:50:14 -0400

    added new components, single card page created

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,18 +1,22 @@
-import React, { Component } from 'react';
-import classnames from 'classnames';
-import { Link } from 'react-router-dom';
-import Nav from '../Nav';
-import SearchForm from '../SearchForm'
-import './style.css';
-import 'bulma';
+import React, { Component } from "react";
+import classnames from "classnames";
+import { Link } from "react-router-dom";
+import Nav from "../Nav";
+import SearchForm from "../SearchForm";
+import SingleCard from "../SingleCard";
+import "./style.css";
+import "bulma";
 
 class App extends Component {
   render() {
     const { className, ...props } = this.props;
     return (
       <div className={classnames("App", className)} {...props}>
-        <Nav />     
-      <SearchForm />
+        <Nav />
+        <div className="pageContent">
+          <SearchForm />
+          <SingleCard />
+        </div>
       </div>
     );
   }
