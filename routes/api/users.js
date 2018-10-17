8e8792edc8187087284740d3234e4ada6d91118b
Daniel McKemie <daniel.mckemie@gmail.com>
2018-10-17 02:13:04 -0400

    built token assignment upon login

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const bcrypt = require('bcryptjs')
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const keys = require('../../config/keys');
 
 // Load User model
 const User = require('../../models/User');
@@ -14,30 +16,67 @@ router.get('/test', (req, res) => res.json({ msg: 'Users Works' }));
 // @desc 	 Register User
 // @access Public
 router.post('/register', (req, res) => {
-	 User.findOne({ email: req.body.email }).then(user => {
-    if (user) {
-      errors.email = 'Email already exists';
-      return res.status(400).json(errors);
-    } else {
-     
+	User.findOne({ email: req.body.email }).then(user => {
+		if (user) {
+			errors.email = 'Email already exists';
+			return res.status(400).json(errors);
+		} else {
+			const newUser = new User({
+				email: req.body.email,
+				password: req.body.password
+			});
 
-      const newUser = new User({        
-        email: req.body.email,
-        password: req.body.password,
-      });
+			bcrypt.genSalt(10, (err, salt) => {
+				bcrypt.hash(newUser.password, salt, (err, hash) => {
+					if (err) throw err;
+					newUser.password = hash;
+					newUser
+						.save()
+						.then(user => res.json(user))
+						.catch(err => console.log(err));
+				});
+			});
+		}
+	});
+});
 
-      bcrypt.genSalt(10, (err, salt) => {
-        bcrypt.hash(newUser.password, salt, (err, hash) => {
-          if (err) throw err;
-          newUser.password = hash;
-          newUser
-            .save()
-            .then(user => res.json(user))
-            .catch(err => console.log(err));
-        });
-      });
-    }
-  });
+// @route  GET api/users/login
+// @desc 	 Login User / Returning JWT Token
+// @access Public
+router.post('/login', (req, res) => {
+	const email = req.body.email;
+	const password = req.body.password;
+
+	// Find user by email
+	User.findOne({ email }).then(user => {
+		// Check for user
+		if (!user) {
+			return res.status(404).json({ email: 'User not found' });
+		}
+
+		// Check Password
+		bcrypt.compare(password, user.password).then(isMatch => {
+			if (isMatch) {
+				// User Matched
+				const payload = { id: user.id, email: user.email }; // Create JWT Payload
+
+				// Sign Token
+				jwt.sign(
+					payload,
+					keys.secretOrKey,
+					{ expiresIn: 3600 },
+					(err, token) => {
+						res.json({
+							success: true,
+							token: 'Bearer ' + token
+						});
+					}
+				);
+			} else {
+				return res.status(400).json({ password: 'Password incorrect' });
+			}
+		});
+	});
 });
 
 module.exports = router;
