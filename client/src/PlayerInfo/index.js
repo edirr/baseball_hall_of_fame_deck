d6455a23506190dfc7f1dfaf3c7f023a849d9b70
Misha Holzman <mcholzman1@gmail.com>
2018-10-19 16:05:34 -0400

    improved PlayerInfo, StatsPitcher, and StatsPostionPlayer components. Styled the singlecard page

--- client/src/PlayerInfo/index.js
+++ client/src/PlayerInfo/index.js
@@ -3,54 +3,77 @@ import Axios from "axios";
 import "./style.css";
 
 class PlayerInfo extends Component {
-  constructor(props){
-  super(props);
-  this.state ={
-    info: []
+  constructor(props) {
+    super(props);
+    this.state = {
+      info: []
+    };
   }
-}
 
-componentDidMount(){
-  const api_end = `http://lookup-service-prod.mlb.com/json/named.search_player_all.bam?sport_code='mlb'&active_sw='N'&name_part='Ted Williams'`
-  const self = this;
-  Axios.get(api_end)
-  .then(function (response) {
-    // console.log(search_player_all.queryResults.row);
+  componentDidMount() {
+    const api_end = `http://lookup-service-prod.mlb.com/json/named.search_player_all.bam?sport_code='mlb'&active_sw='N'&name_part='Ted Williams'`;
+    const self = this;
+    Axios.get(api_end)
+      .then(function(response) {
+        // console.log(search_player_all.queryResults.row);
 
-    let infoData = response.data.search_player_all.queryResults.row;
-    console.log(infoData)
-    const info = {
-      wt: infoData.weight,
-      ht_feet: infoData.height_feet,
-      ht_inch: infoData.height_inches,
-      bats: infoData.bats,
-      throws: infoData.throws,
-      born: infoData.birth_date,
-      debut_date: infoData.pro_debut_date,
-    }
-    console.log(info)
-    self.setState({
-      info:info
-    });
-    // console.log(info);
-  })
-  .catch(function (error) {
-    console.log(error);
-  });
-
-}
-
+        let infoData = response.data.search_player_all.queryResults.row;
+        console.log(infoData);
+        const info = {
+          name: infoData.name_display_first_last,
+          wt: infoData.weight,
+          ht_feet: infoData.height_feet,
+          ht_inch: infoData.height_inches,
+          bats: infoData.bats,
+          throws: infoData.throws,
+          born: infoData.birth_date.split("T")[0],
+          debut_date: infoData.pro_debut_date.split("T")[0]
+        };
+        console.log(info);
+        self.setState({
+          info: info
+        });
+        // console.log(info);
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
+  }
 
   render() {
     const { info } = this.state;
     return (
       <div className="infoContainer">
-        <p><strong>Weight: </strong>{info.wt}</p>
-        <p><strong>Height: </strong>{info.ht_feet}{info.ht_inch}</p>
-        <p><strong>Bats: </strong>{info.bats}</p>
-        <p><strong>Throws: </strong>{info.throws}</p>
-        <p><strong>Born: </strong>{info.born}</p>
-        <p><strong>Date of pro debut: </strong>{info.debut_date}</p>
+        <h1 style={{ paddingBottom: "20px" }}>
+          <strong className="name">{info.name}</strong>
+        </h1>
+        <div id="info">
+          <p>
+            <strong>Weight: </strong>
+            {info.wt}
+          </p>
+          <p>
+            <strong>Height: </strong>
+            {info.ht_feet}
+            {info.ht_inch}
+          </p>
+          <p>
+            <strong>Bats: </strong>
+            {info.bats}
+          </p>
+          <p>
+            <strong>Throws: </strong>
+            {info.throws}
+          </p>
+          <p>
+            <strong>Born: </strong>
+            {info.born}
+          </p>
+          <p>
+            <strong>Date of pro debut: </strong>
+            {info.debut_date}
+          </p>
+        </div>
       </div>
     );
   }
@@ -58,9 +81,8 @@ componentDidMount(){
 
 export default PlayerInfo;
 
-
-
-{/*
+{
+  /*
   render() {
     const { info } = this.state;
 
@@ -80,5 +102,5 @@ export default PlayerInfo;
               <td>{info.rbi}</td>
               <td>{info.sb}</td>
             </tr>
-*/}
-
+*/
+}
