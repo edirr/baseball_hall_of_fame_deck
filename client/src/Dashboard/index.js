d1669c5e7e92a48d4e6602dcb85b5617fc697ed9
Eric Dirr <edirr92@aol.com>
2018-10-22 16:24:53 -0400

    filtered search with db data

--- client/src/Dashboard/index.js
+++ client/src/Dashboard/index.js
@@ -10,28 +10,56 @@ class Dashboard extends Component {
 		super(props);
 		this.state = {
 			name: '',
-			imgUrl: ''
+			imgUrl: '',
+			search: '',
 		};
+		this.updateSearch = this.updateSearch.bind(this);
 	}
 	componentDidMount() {
-		this.props.getCurrentProfile();		
+		this.props.getCurrentProfile();
 	}
-  
+	updateSearch(event){
+  const self = this;
+  self.setState({search: event.target.value});
+}
+
 	render() {
+		// const cards = this.props.profile.profile
+		// let filteredCards = cards.filter(
+  //    (card)=> {
+  //     return card.name.toLowerCase().indexOf(
+  //       this.state.search.toLowerCase()) !== -1;
+  //    }
+  //    );
 		const { profile:{profile : profiles} } = this.props;
 		const { user } = this.props.auth;
 		const { profile } = this.props.profile;
-		console.log(this.props)
+		console.log({profiles});
+
+let filteredCards = !profiles
+	? []
+	: profiles.filter(card => {
+			return (
+				card.name.toLowerCase().indexOf(this.state.search.toLowerCase()) !== -1
+			);
+	  });
+
+
+
 		return (
-			<div>
-				<h3>Dashboard</h3>
-				{
-					!profiles ? [] :
-					profiles.map(({name, imgUrl}) => 
-						<p>{name} <img src={imgUrl} /></p>
-					)
-				}
-			</div>		
+			<div className="container">
+        <hr className="line" />
+        <h1 className="search-heading">Search for a Card</h1>
+        <div className="control tile is-6 search-form ">
+          <input value={this.state.search} onChange={this.updateSearch} className="input" type="text" placeholder="Search Players" />
+        </div>
+        <div className='card-list filtered-names'>{
+          filteredCards.map((card)=>
+            <Link to="/view/card"><img id="player-img" className="card-img" api_id={card.apiId} src={card.imgUrl} style={{ paddingRight: '25px'}}/><p className="player-name"><strong>{card.name}</strong></p></Link>
+          )
+        }</div>
+        <hr className="line" />
+      </div>
 		);
 	}
 }
